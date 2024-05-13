47078dc0b8ae55a4e9587b8e942f149f03b39ea6
T Parker <98612931+tahayparker@users.noreply.github.com>
2024-05-13 14:17:10 +0400

    update

--- Equations.m
+++ Equations.m
@@ -2,26 +2,6 @@
     given initial velocity, angle of launch, initial height, mass of the projectile and acceleration write equations to find final position, coordinates at max height, max distance, and time of flight. 
 %}
 
-function [final_position, max_height_coordinates, max_distance, time_of_flight] = projectile_motion(initial_velocity, angle_of_launch, initial_height, mass, acceleration)
-    % Convert angle from degrees to radians
-    angle_of_launch = deg2rad(angle_of_launch);
-
-    % Time of flight
-    time_of_flight = (2 * initial_velocity * sin(angle_of_launch)) / acceleration;
-
-    % Max distance
-    max_distance = initial_velocity * cos(angle_of_launch) * time_of_flight;
-
-    % Coordinates at max height
-    time_at_max_height = initial_velocity * sin(angle_of_launch) / acceleration;
-    max_height = initial_height + initial_velocity * sin(angle_of_launch) * time_at_max_height - 0.5 * acceleration * time_at_max_height^2;
-    max_height_coordinates = [initial_velocity * cos(angle_of_launch) * time_at_max_height, max_height];
-
-    % Final position
-    final_position = [max_distance, initial_height - 0.5 * acceleration * time_of_flight^2];
-end
-
-% New Equation including air resistance and mass of the projectile
 function [final_position, max_height_coordinates, max_distance, time_of_flight] = projectile_motion(initial_velocity, angle_of_launch, initial_height, mass, acceleration, air_resistance)
     % Convert angle from degrees to radians
     angle_of_launch = deg2rad(angle_of_launch);
@@ -54,6 +34,9 @@ function [final_position, max_height_coordinates, max_distance, time_of_flight] 
 
     % Output
     final_position = [X(end), Y(end)];
+    % Time at which the projectile reaches max height 
+    [~, index] = max(Y);
+    time_at_max_height = index * dt;
     max_height_coordinates = [X(Y == max(Y)), max(Y)];
     max_distance = X(end);
     time_of_flight = length(X) * dt;
