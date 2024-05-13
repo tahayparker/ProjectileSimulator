138175b289eb94370233b7a23df348e90bc1786d
T Parker <98612931+tahayparker@users.noreply.github.com>
2024-05-13 14:17:56 +0400

    update

--- Equations.m
+++ Equations.m
@@ -41,3 +41,28 @@ function [final_position, max_height_coordinates, max_distance, time_of_flight] 
     max_distance = X(end);
     time_of_flight = length(X) * dt;
 end
+
+% OLD CODE
+
+%{
+
+function [final_position, max_height_coordinates, max_distance, time_of_flight] = projectile_motion(initial_velocity, angle_of_launch, initial_height, mass, acceleration)
+    % Convert angle from degrees to radians
+    angle_of_launch = deg2rad(angle_of_launch);
+
+    % Time of flight
+    time_of_flight = (2 * initial_velocity * sin(angle_of_launch)) / acceleration;
+
+    % Max distance
+    max_distance = initial_velocity * cos(angle_of_launch) * time_of_flight;
+
+    % Coordinates at max height
+    time_at_max_height = initial_velocity * sin(angle_of_launch) / acceleration;
+    max_height = initial_height + initial_velocity * sin(angle_of_launch) * time_at_max_height - 0.5 * acceleration * time_at_max_height^2;
+    max_height_coordinates = [initial_velocity * cos(angle_of_launch) * time_at_max_height, max_height];
+
+    % Final position
+    final_position = [max_distance, initial_height - 0.5 * acceleration * time_of_flight^2];
+end
+
+%}
