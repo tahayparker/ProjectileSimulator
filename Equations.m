f84028a6025d012817709672ed90dc488a97598e
T Parker <98612931+tahayparker@users.noreply.github.com>
2024-05-14 16:12:17 +0400

    update

--- Equations.m
+++ Equations.m
@@ -2,51 +2,7 @@
     given initial velocity, angle of launch, initial height, mass of the projectile and acceleration write equations to find final position, coordinates at max height, max distance, and time of flight. 
 %}
 
-function [final_position, max_height_coordinates, max_distance, time_of_flight] = projectile_motion(initial_velocity, angle_of_launch, initial_height, mass, acceleration, air_resistance)
-    % Convert angle from degrees to radians
-    angle_of_launch = deg2rad(angle_of_launch);
-
-    % Initial conditions
-    x = 0; % initial horizontal position
-    y = initial_height; % initial vertical position
-    vx = initial_velocity * cos(angle_of_launch); % initial horizontal velocity
-    vy = initial_velocity * sin(angle_of_launch); % initial vertical velocity
-
-    % Time step
-    dt = 0.01; % seconds
-
-    % Initialize arrays to store the trajectory
-    X = [];
-    Y = [];
-
-    % While the projectile is still in the air
-    while y >= 0
-        % Append current position to the trajectory
-        X = [X x];
-        Y = [Y y];
-
-        % Update velocity and position
-        vx = vx - air_resistance/mass * vx * dt;
-        vy = vy - (acceleration + air_resistance/mass * vy) * dt;
-        x = x + vx * dt;
-        y = y + vy * dt;
-    end
-
-    % Output
-    final_position = [X(end), Y(end)];
-    % Time at which the projectile reaches max height 
-    [~, index] = max(Y);
-    time_at_max_height = index * dt;
-    max_height_coordinates = [X(Y == max(Y)), max(Y)];
-    max_distance = X(end);
-    time_of_flight = length(X) * dt;
-end
-
-% OLD CODE
-
-%{
-
-function [final_position, max_height_coordinates, max_distance, time_of_flight] = projectile_motion(initial_velocity, angle_of_launch, initial_height, mass, acceleration)
+function [final_position, max_height_coordinates, max_distance, time_of_flight] = projectile_motion(initial_velocity, angle_of_launch, initial_height, acceleration)
     % Convert angle from degrees to radians
     angle_of_launch = deg2rad(angle_of_launch);
 
@@ -64,5 +20,3 @@ function [final_position, max_height_coordinates, max_distance, time_of_flight] 
     % Final position
     final_position = [max_distance, initial_height - 0.5 * acceleration * time_of_flight^2];
 end
-
-%}
