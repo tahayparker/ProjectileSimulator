e8240116f32b4be5195982b1801bcf422c265d65
T Parker <98612931+tahayparker@users.noreply.github.com>
2024-05-15 11:24:00 +0400

    update

--- Equations.m
+++ Equations.m
@@ -20,3 +20,63 @@ function [final_position, max_height_coordinates, max_distance, time_of_flight] 
     % Final position
     final_position = [max_distance, initial_height - 0.5 * acceleration * time_of_flight^2];
 end
+
+% Equations to plot a velocity time graph
+
+function velocity_time_graph(initial_velocity, angle_of_launch, acceleration)
+    % Convert angle from degrees to radians
+    angle_of_launch = deg2rad(angle_of_launch);
+
+    % Time of flight
+    time_of_flight = (2 * initial_velocity * sin(angle_of_launch)) / acceleration;
+
+    % Time vector
+    time = linspace(0, time_of_flight, 1000);
+
+    % Velocity vector
+    velocity = initial_velocity - acceleration * time;
+
+    % Plot
+    plot(time, velocity);
+    xlabel('Time (s)');
+    ylabel('Velocity (m/s)');
+    title('Velocity-Time Graph');
+end
+
+% Equations to plot a displacement time graph
+
+function displacement_time_graph(initial_velocity, angle_of_launch, initial_height, acceleration)
+    % Convert angle from degrees to radians
+    angle_of_launch = deg2rad(angle_of_launch);
+
+    % Time of flight
+    time_of_flight = (2 * initial_velocity * sin(angle_of_launch)) / acceleration;
+
+    % Time vector
+    time = linspace(0, time_of_flight, 1000);
+
+    % Displacement vector
+    displacement = initial_velocity * cos(angle_of_launch) * time - 0.5 * acceleration * time.^2 + initial_height;
+
+    % Plot
+    plot(time, displacement);
+    xlabel('Time (s)');
+    ylabel('Displacement (m)');
+    title('Displacement-Time Graph');
+end
+
+% Equations to plot an acceleration time graph
+
+function acceleration_time_graph(acceleration)
+    % Time vector
+    time = linspace(0, 10, 1000);
+
+    % Acceleration vector
+    acceleration = acceleration * ones(1, 1000);
+
+    % Plot
+    plot(time, acceleration);
+    xlabel('Time (s)');
+    ylabel('Acceleration (m/s^2)');
+    title('Acceleration-Time Graph');
+end
