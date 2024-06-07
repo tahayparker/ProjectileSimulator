8ae42e2bfce8daf8c31247a5b0b323a0189c7b05
T Parker <98612931+tahayparker@users.noreply.github.com>
2024-06-07 18:45:34 +0400

    update

--- eqs4.m
+++ eqs4.m
@@ -1,16 +1,18 @@
+% Define the known parameters
+global y_b H x D g d;
+x = input("Enter the distance from the thrower to the building: ");
+H = input("Enter the height of the building: ");
+y_b = 3;    % Height of the basket in meters
+d = 6;      % Distance from building to basket in meters
+D = x + d;  % Total horizontal distance from thrower to basket
+g = 9.81;   % Acceleration due to gravity in m/s^2
+
 % Define the system of equations to solve
 function F = equations(vars)
-    % Define the known parameters
-    y_b = 3;    % Height of the basket in meters
-    H = 5;      % Height of the building in meters (example value)
-    x = 4;      % Distance from thrower to building in meters (example value)
-    d = 6;      % Distance from building to basket in meters
-    D = x + d;  % Total horizontal distance from thrower to basket
-    g = 9.81;   % Acceleration due to gravity in m/s^2
-
+    global y_b H x D g;
     theta = vars(1);
     v0 = vars(2);
-    
+
     % Equation for the vertical position at the basket
     eq1 = y_b - (D * tan(theta) - (1/2) * g * (D / (v0 * cos(theta)))^2);
     
@@ -32,7 +34,7 @@ solution = fsolve(@equations, initial_guess, options);
 theta_solution_rad = solution(1);
 theta_solution_deg = solution(1)*180/pi();
 v0_solution = solution(2);
-t_flight = D / (v0_solution * cos(theta_solution));
+t_flight = D / (v0_solution * cos(theta_solution_rad));
 
 % Display the results
 fprintf('Initial angle (theta): %.2f degrees\n', theta_solution_deg);
